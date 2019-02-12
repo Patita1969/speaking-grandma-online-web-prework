9daf9fef0bf9bdb6bb18f4105c38b1993d4f563b
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-12 06:59:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,18 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma
+  if speak == speak
+    puts “HUH?! SPEAK UP, SONNY"
+    
+  elsif == I LOVE YOUR GRANDMA!
+    
+  else shout == shout
+    puts “NO, NOT SINCE 1938!”
+end
+    
+    
+  
+    
+  
