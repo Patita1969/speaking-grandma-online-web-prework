85db6d774fae4672f19c5c065341789042193667
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-12 06:59:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,14 +15,9 @@
 def speak_to_grandma
   if speak == speak
     puts “HUH?! SPEAK UP, SONNY"
-    
+
   elsif == I LOVE YOUR GRANDMA!
-    
+
   else shout == shout
     puts “NO, NOT SINCE 1938!”
 end
-    
-    
-  
-    
-  
